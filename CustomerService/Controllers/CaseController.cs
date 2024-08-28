4c9bcf70a56e81f2ce4719102d4af52078e757a0
siddharthpd <sidpd510@gmail.com>
2024-08-28 18:46:02 +0530

    Add functionality done

--- CustomerService/Controllers/CaseController.cs
+++ CustomerService/Controllers/CaseController.cs
@@ -58,13 +58,45 @@ namespace CustomerService.Controllers
                 Entity newCase = new Entity("incident");
                 newCase["title"] = model.Title;
                 newCase["description"] = model.Description;
-                newCase["prioritycode"] = model.Priority;
+                newCase["prioritycode"] = new OptionSetValue(int.Parse(model.Priority));
                 if (!string.IsNullOrEmpty(model.Customer))
                 {
                     newCase["customerid"] = new EntityReference("account",Guid.Parse(model.Customer));    
                 }
                 
                 var caseId = _dataverseService.CreateEntity(newCase);
+
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit()
+        {
+            var accounts = _dataverseService.RetrieveEtities("account", new ColumnSet("name", "accountid"));
+
+            ViewBag.Accounts = new SelectList(accounts.Entities.Select(a => new {
+                Id = a.Id,
+                Name = a.GetAttributeValue<string>("name")
+            }), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CaseModel model)
+        {
+
+            Entity newCase = new Entity("incident");
+            newCase["title"] = model.Title;
+            newCase["description"] = model.Description;
+            newCase["prioritycode"] = new OptionSetValue(int.Parse(model.Priority));
+            if (!string.IsNullOrEmpty(model.Customer))
+            {
+                newCase["customerid"] = new EntityReference("account", Guid.Parse(model.Customer));
+            }
+
+            var caseId = _dataverseService.CreateEntity(newCase);
 
 
 
