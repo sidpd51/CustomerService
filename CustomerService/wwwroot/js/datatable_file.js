ca388fdccca0f86aa1564f9e11d57b86eddbd304
siddharthpd <sidpd510@gmail.com>
2024-09-03 10:50:49 +0530

    add logic for visibility of resolve and cancel button only when the status is not equal to resolve or cancel

--- CustomerService/wwwroot/js/datatable_file.js
+++ CustomerService/wwwroot/js/datatable_file.js
@@ -13,7 +13,6 @@ function GetCases() {
 }
 
 function onSuccess(res) {
-    debugger
     $('#dataTable').DataTable({
         bProcessing: true,
         bLengthChange: true,
@@ -57,6 +56,8 @@ function onSuccess(res) {
                 data: "CaseId",
                 render: function (data, type, row, meta) {
                     var isOwner = row.owner === row.userId;
+                    var validStatus = row.status !='Problem Solved'&&row.status!='Cancelled';
+                    var result = isOwner && validStatus;
 
                     return (
                         `
@@ -67,10 +68,10 @@ function onSuccess(res) {
                             <a href="/Case/Delete/${row.caseId}" class="btn btn-danger mx-2 ${!isOwner ? 'disabled' : ''}" >
                                 <i class="bi bi-trash"></i>
                             </a>
-                            <a href="/Case/Resolve/${row.caseId}" class="btn btn-success mx-2 ${!isOwner ? 'disabled' : ''}" >
+                            <a href="/Case/Resolve/${row.caseId}" class="btn btn-success mx-2 ${!result ? 'disabled' : ''}" >
                                 <i class="bi bi-arrow-up-circle-fill"></i>
                             </a>
-                            <a href="/Case/Cancel/${row.caseId}" class="btn btn-warning mx-2 ${!isOwner ? 'disabled' : ''}" >
+                            <a href="/Case/Cancel/${row.caseId}" class="btn btn-warning mx-2 ${!result ? 'disabled' : ''}" >
                                 <i class="bi bi-arrow-down-circle-fill"></i>
                             </a>
                         </div>
